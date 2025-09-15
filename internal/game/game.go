ccf665cfb73130d2ff7b9178bb6f1b20c2203ac3
Gabriel Feyer <gabriel.feyer@indexexchange.com>
2025-09-15 14:22:28 -0400

    refactor: remove random entity spawning from game initialization

--- internal/game/game.go
+++ internal/game/game.go
@@ -1,11 +1,6 @@
 package game
 
 import (
-	"image/color"
-	"math/rand"
-	"time"
-
-	"github.com/gfeyer/ebit/internal/components"
 	"github.com/gfeyer/ebit/internal/settings"
 	"github.com/gfeyer/ebit/internal/systems"
 	"github.com/hajimehoshi/ebiten/v2"
@@ -35,7 +30,6 @@ func NewGame(w, h int) *Game {
 		world: world,
 	}
 
-	spawnRandom(g.world, 32)
 	return g
 }
 
@@ -52,34 +46,4 @@ func (g *Game) Layout(outsideW, outsideH int) (int, int) {
 	entry, _ := SettingsQuery.First(g.world)
 	s := settings.SettingsRes.Get(entry)
 	return s.ScreenWidth, s.ScreenHeight
-}
-
-func spawnRandom(w donburi.World, n int) {
-	entry, _ := SettingsQuery.First(w)
-	s := settings.SettingsRes.Get(entry)
-	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := 0; i < n; i++ {
-		e := w.Create(components.Position, components.Velocity, components.Sprite)
-		entry := w.Entry(e)
-
-		// random position/velocity
-		px := rng.Float64() * float64(s.ScreenWidth-24)
-		py := rng.Float64() * float64(s.ScreenHeight-24)
-		vx := (rng.Float64()*2 - 1) * 120 // px/sec
-		vy := (rng.Float64()*2 - 1) * 120 // px/sec
-
-		*components.Position.Get(entry) = components.Pos{X: px, Y: py}
-		*components.Velocity.Get(entry) = components.Vel{X: vx, Y: vy}
-
-		// tiny colored square sprite
-		size := 16 + rng.Intn(12)
-		img := ebiten.NewImage(size, size)
-		img.Fill(color.RGBA{
-			R: uint8(rng.Intn(80) + 120),
-			G: uint8(rng.Intn(80) + 120),
-			B: uint8(rng.Intn(80) + 120),
-			A: 255,
-		})
-		*components.Sprite.Get(entry) = img
-	}
 }
