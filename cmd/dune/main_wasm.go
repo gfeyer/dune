e663381a07480a647529f8d19ae62d7f7ab53c56
Gabriel Feyer <gabriel.feyer@indexexchange.com>
2025-09-16 08:20:00 -0400

    feat: add WebAssembly build target and web server for browser gameplay

---
+++ cmd/dune/main_wasm.go
@@ -0,0 +1,18 @@
+//go:build js
+
+package main
+
+import (
+	"github.com/gfeyer/ebit/internal/game"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func main() {
+	const W, H = 1280, 720
+	ebiten.SetTPS(60)
+
+	g := game.NewGame(W, H)
+	if err := ebiten.RunGame(g); err != nil {
+		panic(err)
+	}
+}
